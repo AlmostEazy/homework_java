41c2d3a352f79e232ae74de12b86eb9db6fbe829
AlmostEazy <Antosha101992@rambler.ru>
2023-02-14 19:54:20 +0300

    feat: add homework_2 files

---
+++ homework_2/task_1.java
@@ -0,0 +1,36 @@
+//Задание 1.
+//Дана строка sql-запроса "select * from students where ".
+//Сформируйте часть WHERE этого запроса, используя StringBuilder.
+//Данные приведены ниже в виде json строки.
+//Если значение null, то параметр не должен попадать в запрос.
+
+//Входная строка:
+//{"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+
+//Выходная строка:
+//select * from students where name = "Ivanov" and country = "Russia" and city = "Moscow"
+
+
+package homework_2;
+
+public class task_1 {
+    public static void main(String[] args) {
+        String request = "select * from students where ";
+        String dataBase = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"} ";
+        String dataBaseNew = dataBase.replace("{", " ").replace("}", " ");
+        StringBuilder stringBuilder = new StringBuilder(request);
+        String[] dataBaseArr = dataBaseNew.split(",");
+
+        for (int i = 0; i < dataBaseArr.length; i += 1) {
+            String[] arrElem = dataBaseArr[i].replace("\"", " ").split(":");
+            
+            if (!"null".equals(arrElem[1].trim())) {
+                stringBuilder.append(arrElem[0].trim()).append(" = ").append("\"").append(arrElem[1].trim()).append("\"");
+                if (i < dataBaseArr.length - 2)
+                    stringBuilder.append(" and ");
+            }
+        }
+        System.out.println(stringBuilder);
+    }
+}    
+
