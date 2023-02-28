9daa0e145e9c5fbb53ddbe8fd7f19f91f7d875c3
AlmostEazy <Antosha101992@rambler.ru>
2023-02-28 23:27:07 +0300

    feat: add homework_6 files

--- homework_6/notebooksInfo.java
+++ homework_6/notebooksInfo.java
@@ -1,33 +1,27 @@
 package homework_6;
 
-import java.util.Scanner;
-
+import java.util.HashSet;
+import java.util.Set;
 
 public class notebooksInfo {
-    private static final String MENU = "Выберите нужный пункт меню: "
-            + "\n\t1 - Показать все ноутбуки"
-            + "\n\t2 - Искать"
-            + "\n\t5 - Вернуться назад, в главное меню"
-            + "\n\t0 - Выход";
+    public static void main(String[] args) 
+    {
+        System.out.println("\n Добро пожаловать в каталог ноутбуков!");
+        Set<laptopPC> set = new HashSet<>();
+        set.add(new laptopPC
+        ("001", 32, "Mac", 1024, "Apple"));
+        set.add(new laptopPC
+        ("002", 16, "Windows", 1024, "HUAWEI"));
+        set.add(new laptopPC
+        ("003", 12, "linux", 256, "Maibenben"));
+        set.add(new laptopPC
+        ("004", 8, "Windows", 512, "Acer"));
+        set.add(new laptopPC
+        ("005", 4, "Windows", 128, "Irbis"));
+    
+        program operation = new program(set);
+        operation.start();
+        System.out.println("\nПриходите еще!");
 
-    public static void taskMenuSeminar6() {
-        boolean itContinues = true;
-        while (itContinues) {
-            System.out.println(MENU);
-            switch (Scanner.getIntFromInput("Выберите задачу: ")) {
-                case 1:
-                    TaskSem6.viewAllLaptop();
-                    break;
-                case 2:
-                    TaskSem6.viewFilterLaptop();
-                    break;
-                case 0:
-                    System.exit(0);
-                default:
-                    System.out.println("Такого действия не существует!");
-                    break;
-            }
-        }
-        ScannerHelper.close();
     }
 }
--- homework_6/program.java
+++ homework_6/program.java
@@ -1,54 +1,224 @@
 package homework_6;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class program {
-    public static void main(String[] args) {
+    private Set<laptopPC> notebooks = new HashSet<>();
+    private List<Criterion> criterionList = new ArrayList<>();
+    private static Scanner sc = new Scanner(System.in);
 
-        List<laptopPC> notebooks = new ArrayList<>();
-       
-        notebooks.add(new laptopPC(notebooks.size(), "Apple", "MacBook Pro", "Silver", 13.3, "M2", "SSD", 3.2,
-                        32, 1024,
-                        "MacOS"));
-        notebooks.add(
-                        new laptopPC(notebooks.size(), "Huawei", "MateBook 14", "Grey", 14.2, "AMD Ryzen",
-                                        "SSD", 2.2, 16, 512,
-                                        "Windows 10"));
-        notebooks.add(
-                        new laptopPC(notebooks.size(), "Asus", "Vivobook Go 14", "Black", 14, "Intel Pentium",
-                                        "SSD", 1.1, 8,
-                                        256, "no OS"));
-        notebooks.add(
-                        new laptopPC(notebooks.size(), "Huawei", "MateBook D14", "Silver", 14, "Intel Core",
-                                        "SSD", 2.4, 8, 512,
-                                        "Windows"));
-        notebooks.add(new laptopPC(notebooks.size(), "Hauwei", "MateBook D 15", "Grey",
-                        15.6, "Intel Core", "SSD", 2.8, 16, 512, "Windows"));
-        notebooks.add(new laptopPC(notebooks.size(), "Asus", "TUF Gaming F15",
-                        "Grey", 15.6, "Intel Core", "SSD", 2.7, 16, 512, "no OS"));
-        notebooks.add(
-                        new laptopPC(notebooks.size(), "Huawei", "MateBook D16", "Space grey", 16, "Intel Core",
-                                        "SSD", 3.3,
-                                        16, 512, "Windows"));
-        notebooks.add(new laptopPC(notebooks.size(), "Honor", "MagicBook 16", "Space grey",
-                        16.1, "AMD Ryzen", "SSD", 3.3, 16, 512, "Windows"));
-        notebooks.add(new laptopPC(notebooks.size(), "Apple", "MacBook Pro 14",
-                        "Space grey", 14.2, "M1", "SSD", 3.2, 16, 1024, "MacOS"));
-        notebooks.add(new laptopPC(notebooks.size(), "Apple", "MacBook Air",
-                        "Black", 13.6, "M2", "SSD", 3.5, 8, 256, "MacOS"));
-
+    public void printList() {
         for (laptopPC notebook : notebooks) {
-                System.out.println(notebook.getInfo());
-                System.out.println();
+            if (notebookIsCorrect(notebook)) {
+                System.out.println(notebook);
+            }
+        }
+    }
+
+    public boolean notebookIsCorrect(laptopPC notebook) {
+        for (Criterion criterion : criterionList) {
+            Object valueNotebook = null;
+
+            if (criterion.property.equals("name")) {
+                valueNotebook = notebook.getId();
+            } else if (criterion.property.equals("RAMamount")) {
+                valueNotebook = notebook.getRam();
+            } else if (criterion.property.equals("operatingSystem")) {
+                valueNotebook = notebook.getOperatingSystem();
+            } else if (criterion.property.equals("hard")) {
+                valueNotebook = notebook.getHard();
+            } else if (criterion.property.equals("model")) {
+                valueNotebook = notebook.getModel();
+            } else {
+                continue;
+            }
+
+            if (criterion.value != null && !criterion.value.equals(valueNotebook)) {
+                return false;
+            }
+
+            if (criterion.maxValue != null
+                    && criterion.maxValue < Double.parseDouble(Objects.toString(valueNotebook))) {
+                return false;
+            }
+
+            if (criterion.minValue != null
+                    && criterion.minValue > Double.parseDouble(Objects.toString(valueNotebook))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public program(Set<laptopPC> notebooks) {
+        this.sc = new Scanner(System.in);
+        this.notebooks = notebooks;
+    }
+
+    public program(Set<laptopPC> notebooks, List<Criterion> criterionList) {
+        this.sc = new Scanner(System.in);
+        this.notebooks = notebooks;
+        this.criterionList = criterionList;
+    }
+
+    public int getCriteria() {
+        String text = "Введите цифру, соответствующую критерию поиска: ";
+        List<String> properties = filterProperties();
+
+        for (int i = 0; i < properties.size(); i++) {
+            text += "\n" + (i + 1) + ". " + getPropertyDescription(properties.get(i));
+        }
+        System.out.println(text);
+        System.out.print("необходимый критерий: ");
+        int value = sc.nextInt();
+        return value;
+    }
+
+    public String getPropertyDescription(String property) {
+        Map<String, String> descriptionsProperties = descriptionsProperties();
+        return descriptionsProperties.get(property);
+    }
+
+    public Map<String, String> descriptionsProperties() {
+        Map<String, String> map = new HashMap<>();
+
+        map.put("name", "По Id");
+        map.put("RAMamount", "Объем оперативной памяти");
+        map.put("operatingSystem", "Операционная система");
+        map.put("hard", "Объем ЖД");
+        map.put("model", "Модель");
+        return map;
+    }
+
+    public List<String> filterProperties() {
+        List<String> list = new ArrayList<>();
+        list.add("name");
+        list.add("RAMamount");
+        list.add("operatingSystem");
+        list.add("hard");
+        list.add("model");
+        return list;
+    }
+
+    public String getOperations() {
+        String text = " Выберите действие: \n " +
+                "1. Выбор параметра \n " +
+                "2. Список ноутбуков\n " +
+                "3. Выход";
+        System.out.println(text);
+        System.out.print(" Сделайте выбор: ");
+        String answer = sc.next();
+        return answer;
+    }
+
+    public Set<String> quantitativeSelection() {
+        Set<String> set = new HashSet<>();
+        set.add("RAMamount");
+        set.add("hard");
+        return set;
+    }
+
+    public Set<String> stringSelection() {
+        Set<String> set = new HashSet<>();
+        set.add("name");
+        set.add("operatingSystem");
+        set.add("model");
+        return set;
+    }
+
+    public void start() {
+        boolean flag = true;
+        while (flag) {
+            String operation = getOperations();
+            if (operation.equals("3")) {
+                flag = false;
+                sc.close();
+                continue;
+            } else if (operation.equals("1")) {
+
+                int criterion = getCriteria();
+                List<String> properties = filterProperties();
+                if (criterion - 1 < 0 || criterion - 1 > properties.size() - 1) {
+                    System.out.println("Введено некорректное значение");
+                    continue;
+                }
+                String property = properties.get(criterion - 1);
+                Criterion criterionObject = null;
+                try {
+                    if (quantitativeSelection().contains(property)) {
+                        criterionObject = Criterion.startGetting(sc, property, true);
+                    } else {
+                        criterionObject = Criterion.startGetting(sc, property, false);
+                    }
+                } catch (Exception e) {
+                    System.out.println("Ошибка при выборе");
+                    continue;
+                }
+                if (criterionObject != null) {
+                    System.out.println("Ваш критерий добавлен!");
+                    criterionList.add(criterionObject);
+                    System.out.println("Промежуточный итог: ");
+                    printList();
+                }
+            } else if (operation.equals("2")) {
+                printList();
+            }
+        }
+    }
+}
+
+class Criterion {
+
+    Object value;
+    Double minValue;
+    Double maxValue;
+    boolean isQuantitative;
+    String property;
+
+    public Criterion(String property, boolean isQuantitative, Object value, Double minValue, Double maxValue) {
+        this.property = property;
+        this.isQuantitative = isQuantitative;
+        this.value = value;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+    }
+
+    public static Criterion startGetting(Scanner sc, String property, boolean isQuantitative) {
+        if (isQuantitative) {
+            String quest = "Выберите тип критерия: " +
+                    "\n 1. По значению" +
+                    "\n 2. Меньше заданного" +
+                    "\n 3. Больше заданного" +
+                    "\n 4. Интервал";
+            System.out.println(quest);
+            System.out.print("Выбор : ");
+            String text = sc.next();
+            Criterion criterion = null;
+            if (text.equals("1")) {
+                System.out.print("Введите значение : ");
+                int getValue = sc.nextInt();
+                criterion = new Criterion(property, isQuantitative, getValue, null, null);
+            } else if (text.equals("2")) {
+                System.out.print("Введите максимальное предельное значение : ");
+                double getValue = sc.nextDouble();
+                criterion = new Criterion(property, isQuantitative, null, null, getValue);
+            } else if (text.equals("3")) {
+                System.out.print("Введите минимальное предельное значение : ");
+                double getValue = sc.nextDouble();
+                criterion = new Criterion(property, isQuantitative, null, getValue, null);
+            } else if (text.equals("4")) {
+                System.out.print("Введите минимальное предельное значение : ");
+                double getMin = sc.nextDouble();
+                System.out.print("Введите максимальное предельное значение : ");
+                double getMax = sc.nextDouble();
+                criterion = new Criterion(property, isQuantitative, null, getMin, getMax);
+            }
+
+            return criterion;
         }
 
-        notebooksInfo notebookList = new notebooksInfo(notebooks);
-        List<laptopPC> filterList = notebookList.View();
-        System.out.println(filterList.size());
-        for (laptopPC notebook : filterList) {
-                System.out.println(notebook.getInfo());
-                System.out.println();
-        }
+        System.out.print("Введите значение: ");
+        String getValue = sc.next();
+        return new Criterion(property, isQuantitative, getValue, null, null);
     }
 }
